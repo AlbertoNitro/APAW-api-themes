c508f89f7a1ea83230bc22f97f43c79bcaf22f64
JBernal <j.bernal@upm.es>
2017-09-20 06:31:21 +0200

    script travis

--- src/test/java/es/upm/miw/apaw/theme/api/entities/ThemeTest.java
+++ src/test/java/es/upm/miw/apaw/theme/api/entities/ThemeTest.java
@@ -1,6 +1,7 @@
 package es.upm.miw.apaw.theme.api.entities;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
@@ -9,6 +10,7 @@ public class ThemeTest {
     @Test
     public void testGetDate() {
         assertNotNull(new Theme("tema1").getDate());
+        fail();
     }
 
 }
